a9b7da524641d037629939ff5757e52f6a7f0c68
adriano alves <33916451+adrianoportodf2017@users.noreply.github.com>
2024-04-14 13:45:14 -0300

    teste

--- script.js
+++ script.js
@@ -2,21 +2,26 @@ const html = document.documentElement;
 const canvas = document.getElementById("hero-lightpass");
 const context = canvas.getContext("2d");
 
-const frameCount = 479;
+const frameCount = 500;
 const currentFrame = (index) =>
-  `gif/frame_${index.toString().padStart(5, "0")}.gif`;
+`gif/frame_${index.toString().padStart(5, "0")}.gif`;
 
-const preloadImages = () => {
-  for (let i = 1; i < frameCount; i++) {
-    const img = new Image();
-    img.src = currentFrame(i);
-  }
-};
+let imgIndex = 1; // Índice da imagem atual
+let imgLoaded = false; // Flag para controlar se a imagem atual foi carregada
 
 const img = new Image();
-img.src = currentFrame(1);
+
+const loadImage = (index) => {
+  img.src = currentFrame(index);
+  img.onload = () => {
+    imgLoaded = true;
+    drawImage();
+  };
+};
 
 const drawImage = () => {
+  if (!imgLoaded) return; // Se a imagem atual não estiver carregada, saia da função
+
   const windowWidth = window.innerWidth;
   const windowHeight = window.innerHeight;
   const aspectRatio = img.width / img.height;
@@ -24,15 +29,25 @@ const drawImage = () => {
   let imgWidth, imgHeight, offsetX, offsetY;
 
   if (canvasAspectRatio > aspectRatio) {
+    // Se a proporção da tela for maior que a proporção da imagem
     imgWidth = windowWidth;
     imgHeight = imgWidth / aspectRatio;
     offsetX = 0;
     offsetY = (windowHeight - imgHeight) / 2;
   } else {
+    // Se a proporção da tela for menor que a proporção da imagem
     imgHeight = windowHeight;
     imgWidth = imgHeight * aspectRatio;
     offsetY = 0;
     offsetX = (windowWidth - imgWidth) / 2;
+  }
+
+  // Verifica se a tela é ultralarga e ajusta a altura da imagem, se necessário
+  if (canvasAspectRatio > 2) {
+    imgHeight = windowHeight;
+    imgWidth = imgHeight * aspectRatio;
+    offsetX = (windowWidth - imgWidth) / 2;
+    offsetY = 0;
   }
 
   canvas.width = windowWidth;
@@ -40,13 +55,10 @@ const drawImage = () => {
   context.drawImage(img, offsetX, offsetY, imgWidth, imgHeight);
 };
 
-img.onload = drawImage;
-
-window.addEventListener("resize", drawImage);
-
 const updateImage = (index) => {
-  img.src = currentFrame(index);
-  //drawImage();
+  if (!imgLoaded || index === imgIndex) return; // Se a imagem atual não estiver carregada ou for a mesma imagem, não faça nada
+  imgIndex = index;
+  loadImage(imgIndex);
 };
 
 window.addEventListener("scroll", () => {
@@ -58,7 +70,8 @@ window.addEventListener("scroll", () => {
     Math.ceil(scrollFraction * frameCount)
   );
 
-  requestAnimationFrame(() => updateImage(frameIndex + 1));
+  updateImage(frameIndex + 1);
 });
 
-preloadImages();
+// Inicializa carregando a primeira imagem
+loadImage(imgIndex);
